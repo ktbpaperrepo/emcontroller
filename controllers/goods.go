97eec2b40a4d57377e309d115ff51d8ed65c27cb
Weifan Zhang <weifanzh@es.aau.dk>
2022-08-25 18:26:14 +0200

    form data from PUT and DELETE

--- controllers/goods.go
+++ controllers/goods.go
@@ -1,6 +1,9 @@
 package controllers
 
 import (
+	"fmt"
+	"strconv"
+
 	"github.com/astaxie/beego"
 )
 
@@ -8,8 +11,37 @@ type GoodsController struct {
 	beego.Controller
 }
 
-func (c *GoodsController) Get() {
+func (c *GoodsController) Get() { // get
 	c.Data["mytitle"] = "Hello Beego!"
 	c.Data["num"] = 12
 	c.TplName = "goods.tpl"
+}
+
+func (c *GoodsController) DoAdd() { // post
+	c.Ctx.WriteString("execute add operation")
+}
+
+type Product struct {
+	Title   string `form:"title"`
+	Content string `form:"content"`
+}
+
+func (c *GoodsController) DoEdit() { // put
+	//title := c.GetString("title")
+	p := Product{}
+	if err := c.ParseForm(&p); err != nil {
+		c.Ctx.WriteString("error")
+		return
+	}
+	fmt.Printf("%#v\n", p)
+
+	c.Ctx.WriteString("execute edit operation" + p.Title + p.Content)
+}
+
+func (c *GoodsController) DoDelete() { // delete
+	id, err := c.GetInt("id")
+	if err != nil {
+		c.Ctx.WriteString("parameter error")
+	}
+	c.Ctx.WriteString("execute delete operation----" + strconv.Itoa(id))
 }
--- routers/router.go
+++ routers/router.go
@@ -7,14 +7,20 @@ import (
 
 func init() {
 	beego.Router("/", &controllers.MainController{})
-	beego.Router("/goods", &controllers.GoodsController{})
+
 	beego.Router("/article", &controllers.ArticleController{})                       // get method
 	beego.Router("/article/add", &controllers.ArticleController{}, "get:AddArticle") // customized
 	beego.Router("/article/edit", &controllers.ArticleController{}, "get:EditArticle")
+
 	beego.Router("/user", &controllers.UserController{})
 	beego.Router("/user/add", &controllers.UserController{}, "get:AddUser")
 	beego.Router("/user/doAdd", &controllers.UserController{}, "post:DoAddUser")
 	beego.Router("/user/edit", &controllers.UserController{}, "get:EditUser")
 	beego.Router("/user/doEdit", &controllers.UserController{}, "post:DoEditUser")
 	beego.Router("/user/getUser", &controllers.UserController{}, "get:GetUser")
+
+	beego.Router("/goods", &controllers.GoodsController{})
+	beego.Router("/goods/add", &controllers.GoodsController{}, "post:DoAdd")
+	beego.Router("/goods/edit", &controllers.GoodsController{}, "put:DoEdit")
+	beego.Router("/goods/delete", &controllers.GoodsController{}, "delete:DoDelete")
 }
