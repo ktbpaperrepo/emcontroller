85f3ff1aacc26d6dcedf0a297420faba67befa75
Weifan Zhang <weifanzh@es.aau.dk>
2023-01-19 18:03:02 +0100

    add function to list/get/create/delete volume and server on openstack

--- models/openstack.go
+++ models/openstack.go
@@ -2,11 +2,15 @@ package models
 
 import (
 	"fmt"
+
 	"github.com/astaxie/beego"
 	"github.com/gophercloud/gophercloud"
 	"github.com/gophercloud/gophercloud/openstack"
 	storagequota "github.com/gophercloud/gophercloud/openstack/blockstorage/extensions/quotasets"
+	"github.com/gophercloud/gophercloud/openstack/blockstorage/v1/volumes"
 	computequota "github.com/gophercloud/gophercloud/openstack/compute/v2/extensions/quotasets"
+	"github.com/gophercloud/gophercloud/openstack/compute/v2/flavors"
+	"github.com/gophercloud/gophercloud/openstack/compute/v2/servers"
 	networkquota "github.com/gophercloud/gophercloud/openstack/networking/v2/extensions/quotas"
 )
 
@@ -14,6 +18,10 @@ type Openstack struct {
 	Name          string
 	Type          string
 	ProjectID     string
+	ImageID       string // we use a fixed image to create VMs
+	NetworkID     string // we use a fixed network to create VMs
+	SecurityGroup string // we use a fixed security group to create VMs
+	KeyName       string // we use a fixed key pair to create VMs
 	Provider      *gophercloud.ProviderClient
 	ComputeClient *gophercloud.ServiceClient
 	NetworkClient *gophercloud.ServiceClient
@@ -64,6 +72,10 @@ func InitOpenstack(paras map[string]interface{}) *Openstack {
 		Name:          paras["name"].(string),
 		Type:          paras["type"].(string),
 		ProjectID:     paras["project_id"].(string),
+		ImageID:       paras["imageid"].(string),
+		NetworkID:     paras["networkid"].(string),
+		SecurityGroup: paras["securitygroup"].(string),
+		KeyName:       paras["keyname"].(string),
 		Provider:      provider,
 		ComputeClient: computeClient,
 		NetworkClient: networkClient,
@@ -130,4 +142,104 @@ func (os *Openstack) GetStorageQuota() storagequota.QuotaUsageSet {
 		beego.Error(fmt.Sprintf("Cloud name [%s], type [%s], project id [%s], Check storage quota error: %s", os.Name, os.Type, os.ProjectID, err.Error()))
 	}
 	return extracted
+}
+
+// the listed volumes only have the attribute ID, other attributes are empty, so we should use GetVolume to see the details of each one. See the unit test.
+func (os *Openstack) ListAllVolumes() []volumes.Volume {
+	opts := volumes.ListOpts{}
+	allPages, err := volumes.List(os.StorageClient, opts).AllPages()
+	if err != nil {
+		beego.Error(fmt.Sprintf("Cloud name [%s], type [%s], project id [%s], list volumes error: %s", os.Name, os.Type, os.ProjectID, err.Error()))
+	}
+	allVolumes, err := volumes.ExtractVolumes(allPages)
+	if err != nil {
+		beego.Error(fmt.Sprintf("Cloud name [%s], type [%s], project id [%s], volumes.ExtractVolumes error: %s", os.Name, os.Type, os.ProjectID, err.Error()))
+	}
+	return allVolumes
+}
+
+func (os *Openstack) CreateVolume(opts volumes.CreateOpts) *volumes.Volume {
+	beego.Info(fmt.Sprintf("Create volume opts: [%+v]", opts))
+	vol, err := volumes.Create(os.StorageClient, opts).Extract()
+	if err != nil {
+		beego.Error(fmt.Sprintf("Cloud name [%s], type [%s], project id [%s], Create Volume opts [%+v] error: %s", os.Name, os.Type, os.ProjectID, opts, err.Error()))
+	} else {
+		beego.Info(fmt.Sprintf("Successful! Create volume opts: [%+v]", opts))
+	}
+	return vol
+}
+
+func (os *Openstack) GetVolume(id string) *volumes.Volume {
+	vol, err := volumes.Get(os.StorageClient, id).Extract()
+	if err != nil {
+		beego.Error(fmt.Sprintf("Cloud name [%s], type [%s], project id [%s], Get Volume id [%s] error: %s", os.Name, os.Type, os.ProjectID, id, err.Error()))
+	}
+	return vol
+}
+
+func (os *Openstack) DeleteVolume(id string) {
+	beego.Info(fmt.Sprintf("Delete volume id: [%s]", id))
+	res := volumes.Delete(os.StorageClient, id)
+	beego.Info(fmt.Sprintf("Delete volume id: [%s] response:\n%+v\n", id, res))
+	if res.Err != nil {
+		beego.Error(fmt.Sprintf("Cloud name [%s], type [%s], project id [%s], Delete Volume id [%s] error: %s", os.Name, os.Type, os.ProjectID, id, res.Err.Error()))
+	} else {
+		beego.Info(fmt.Sprintf("Successful! Delete volume id: [%s]", id))
+	}
+}
+
+func (os *Openstack) ListAllFavors() []flavors.Flavor {
+	opts := flavors.ListOpts{}
+	allPages, err := flavors.ListDetail(os.ComputeClient, opts).AllPages()
+	if err != nil {
+		beego.Error(fmt.Sprintf("Cloud name [%s], type [%s], project id [%s], list flavors error: %s", os.Name, os.Type, os.ProjectID, err.Error()))
+	}
+	allFlavors, err := flavors.ExtractFlavors(allPages)
+	if err != nil {
+		beego.Error(fmt.Sprintf("Cloud name [%s], type [%s], project id [%s], flavors.ExtractFlavors error: %s", os.Name, os.Type, os.ProjectID, err.Error()))
+	}
+	return allFlavors
+}
+
+func (os *Openstack) ListAllServers() []servers.Server {
+	opts := servers.ListOpts{}
+	allPages, err := servers.List(os.ComputeClient, opts).AllPages()
+	if err != nil {
+		beego.Error(fmt.Sprintf("Cloud name [%s], type [%s], project id [%s], list servers error: %s", os.Name, os.Type, os.ProjectID, err.Error()))
+	}
+	allServers, err := servers.ExtractServers(allPages)
+	if err != nil {
+		beego.Error(fmt.Sprintf("Cloud name [%s], type [%s], project id [%s], servers.ExtractServers error: %s", os.Name, os.Type, os.ProjectID, err.Error()))
+	}
+	return allServers
+}
+
+func (os *Openstack) CreateServer(opts servers.CreateOptsBuilder) *servers.Server {
+	beego.Info(fmt.Sprintf("Create server opts: [%+v]", opts))
+	newServer, err := servers.Create(os.ComputeClient, opts).Extract()
+	if err != nil {
+		beego.Error(fmt.Sprintf("Cloud name [%s], type [%s], project id [%s], Create Server opts [%+v] error: %s", os.Name, os.Type, os.ProjectID, opts, err.Error()))
+	} else {
+		beego.Info(fmt.Sprintf("Successful! Create server opts: [%+v]", opts))
+	}
+	return newServer
+}
+
+func (os *Openstack) GetServer(id string) *servers.Server {
+	server, err := servers.Get(os.ComputeClient, id).Extract()
+	if err != nil {
+		beego.Error(fmt.Sprintf("Cloud name [%s], type [%s], project id [%s], Get Server id [%s] error: %s", os.Name, os.Type, os.ProjectID, id, err.Error()))
+	}
+	return server
+}
+
+func (os *Openstack) DeleteServer(id string) {
+	beego.Info(fmt.Sprintf("Delete server id: [%s]", id))
+	res := servers.Delete(os.ComputeClient, id)
+	beego.Info(fmt.Sprintf("Delete server id: [%s] response:\n%+v\n", id, res))
+	if res.Err != nil {
+		beego.Error(fmt.Sprintf("Cloud name [%s], type [%s], project id [%s], Delete Server id [%s] error: %s", os.Name, os.Type, os.ProjectID, id, res.Err.Error()))
+	} else {
+		beego.Info(fmt.Sprintf("Successful! Delete server id: [%s]", id))
+	}
 }
--- models/openstack_test.go
+++ models/openstack_test.go
@@ -2,12 +2,17 @@ package models
 
 import (
 	"fmt"
+	"testing"
+
 	"github.com/gophercloud/gophercloud"
 	"github.com/gophercloud/gophercloud/openstack"
 	storagequota "github.com/gophercloud/gophercloud/openstack/blockstorage/extensions/quotasets"
+	"github.com/gophercloud/gophercloud/openstack/blockstorage/v1/volumes"
+	"github.com/gophercloud/gophercloud/openstack/compute/v2/extensions/bootfromvolume"
+	"github.com/gophercloud/gophercloud/openstack/compute/v2/extensions/keypairs"
 	computequota "github.com/gophercloud/gophercloud/openstack/compute/v2/extensions/quotasets"
+	"github.com/gophercloud/gophercloud/openstack/compute/v2/servers"
 	networkingquota "github.com/gophercloud/gophercloud/openstack/networking/v2/extensions/quotas"
-	"testing"
 )
 
 func TestGophercloud(t *testing.T) {
@@ -84,6 +89,142 @@ func TestGetStorageQuota(t *testing.T) {
 		switch Clouds[i].(type) {
 		case *Openstack:
 			fmt.Printf("%+v\n", Clouds[i].(*Openstack).GetStorageQuota())
+		}
+	}
+}
+
+func TestListAllVolumes(t *testing.T) {
+	InitClouds()
+	for i := 0; i < len(Clouds); i++ {
+		switch Clouds[i].(type) {
+		case *Openstack:
+			allVolumes := Clouds[i].(*Openstack).ListAllVolumes()
+			for j := 0; j < len(allVolumes); j++ {
+				fmt.Printf("%+v\n", Clouds[i].(*Openstack).GetVolume(allVolumes[j].ID))
+			}
+		}
+	}
+}
+
+func TestCreateVolume(t *testing.T) {
+	InitClouds()
+	for i := 0; i < len(Clouds); i++ {
+		switch Clouds[i].(type) {
+		case *Openstack:
+			opts := volumes.CreateOpts{
+				Size:    150,
+				Name:    "volume-unittest2",
+				ImageID: Clouds[i].(*Openstack).ImageID,
+			}
+			vol := Clouds[i].(*Openstack).CreateVolume(opts)
+			fmt.Printf("%+v\n", vol)
+		}
+	}
+}
+
+func TestGetVolume(t *testing.T) {
+	InitClouds()
+	for i := 0; i < len(Clouds); i++ {
+		switch Clouds[i].(type) {
+		case *Openstack:
+			id := "3dcc23ec-ecf1-460c-9c90-890acd077367"
+			vol := Clouds[i].(*Openstack).GetVolume(id)
+			fmt.Printf("%+v\n", vol)
+		}
+	}
+}
+
+func TestDeleteVolume(t *testing.T) {
+	InitClouds()
+	for i := 0; i < len(Clouds); i++ {
+		switch Clouds[i].(type) {
+		case *Openstack:
+			id := "3dcc23ec-ecf1-460c-9c90-890acd077367"
+			Clouds[i].(*Openstack).DeleteVolume(id)
+		}
+	}
+}
+
+func TestListAllFavors(t *testing.T) {
+	InitClouds()
+	for i := 0; i < len(Clouds); i++ {
+		switch Clouds[i].(type) {
+		case *Openstack:
+			allFlavors := Clouds[i].(*Openstack).ListAllFavors()
+			for j := 0; j < len(allFlavors); j++ {
+				fmt.Printf("%+v\n", allFlavors[j])
+			}
+		}
+	}
+}
+
+func TestListAllServers(t *testing.T) {
+	InitClouds()
+	for i := 0; i < len(Clouds); i++ {
+		switch Clouds[i].(type) {
+		case *Openstack:
+			allServers := Clouds[i].(*Openstack).ListAllServers()
+			for j := 0; j < len(allServers); j++ {
+				fmt.Printf("%+v\n", allServers[j])
+			}
+		}
+	}
+}
+
+func TestCreateServer(t *testing.T) {
+	InitClouds()
+	for i := 0; i < len(Clouds); i++ {
+		switch Clouds[i].(type) {
+		case *Openstack:
+			baseOpts := servers.CreateOpts{
+				Name:           "unittest2",
+				FlavorRef:      "abb9477b-955c-45fa-bfaf-b53ebc8b2cb7",
+				SecurityGroups: []string{Clouds[i].(*Openstack).SecurityGroup},
+				Networks: []servers.Network{
+					{UUID: Clouds[i].(*Openstack).NetworkID},
+				},
+			}
+			optsWithKeyPair := keypairs.CreateOptsExt{
+				CreateOptsBuilder: baseOpts,
+				KeyName:           Clouds[i].(*Openstack).KeyName,
+			}
+			optsBfv := bootfromvolume.CreateOptsExt{
+				CreateOptsBuilder: optsWithKeyPair,
+				BlockDevice: []bootfromvolume.BlockDevice{
+					{
+						BootIndex:           0,
+						DeleteOnTermination: false,
+						UUID:                "7f4d550f-08e2-44f5-8aaf-2ed3a639e5a3",
+						SourceType:          bootfromvolume.SourceVolume,
+						DestinationType:     bootfromvolume.DestinationVolume,
+					},
+				},
+			}
+			newServer := Clouds[i].(*Openstack).CreateServer(optsBfv)
+			fmt.Printf("%+v\n", newServer)
+		}
+	}
+}
+
+func TestGetServer(t *testing.T) {
+	InitClouds()
+	for i := 0; i < len(Clouds); i++ {
+		switch Clouds[i].(type) {
+		case *Openstack:
+			id := "5dff3877-7412-4a21-8932-be4e8f6b566e"
+			vol := Clouds[i].(*Openstack).GetServer(id)
+			fmt.Printf("%+v\n", vol)
+		}
+	}
+}
+
+func TestDeleteServer(t *testing.T) {
+	InitClouds()
+	for i := 0; i < len(Clouds); i++ {
+		switch Clouds[i].(type) {
+		case *Openstack:
+			id := "dd7470ad-c66c-4715-8f7a-ad3b6fbef6b7"
+			Clouds[i].(*Openstack).DeleteServer(id)
 		}
 	}
 }
