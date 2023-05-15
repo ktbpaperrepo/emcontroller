702aeaacbddc261f5ff3fd8ee6e8dda5cf48d614
Weifan Zhang <weifanzh@es.aau.dk>
2023-05-15 14:57:16 +0200

    Add a featureGate of network performance test, to make development, test, and debug easier.

--- main.go
+++ main.go
@@ -18,16 +18,23 @@ func main() {
 	// viper is case-insensitive, so all keys in iaas.json should be lowercase
 	models.InitClouds()
 
-	// When multi-cloud manager starts up, we do measure the network performance once instantly, because we need the network performance information to schedule applications when deploying them.
-	models.MeasNetPerf()
-	// periodically measure the network performance
-	netTestPeriodSec, err := strconv.Atoi(beego.AppConfig.String("NetTestPeriodSec"))
-	if err != nil {
-		beego.Error(fmt.Sprintf("Read config \"NetTestPeriodSec\" error: %s, set the period as the DefaultNetTestPeriodSec", err.Error()))
-		netTestPeriodSec = models.DefaultNetTestPeriodSec
+	if NetTestOn, err := beego.AppConfig.Bool("TurnOnNetTest"); err == nil && NetTestOn {
+		beego.Info("Network performance test function is on.")
+		// When multi-cloud manager starts up, we do measure the network performance once instantly, because we need the network performance information to schedule applications when deploying them.
+		models.MeasNetPerf()
+		// periodically measure the network performance
+		netTestPeriodSec, err := strconv.Atoi(beego.AppConfig.String("NetTestPeriodSec"))
+		if err != nil {
+			beego.Error(fmt.Sprintf("Read config \"NetTestPeriodSec\" error: %s, set the period as the DefaultNetTestPeriodSec", err.Error()))
+			netTestPeriodSec = models.DefaultNetTestPeriodSec
+		}
+		beego.Info(fmt.Sprintf("The period of measuring network performance is %d seconds.", netTestPeriodSec))
+		go models.CronTaskTimer(models.MeasNetPerf, time.Duration(netTestPeriodSec)*time.Second)
+	} else if err != nil {
+		beego.Error(fmt.Sprintf("Read \"TurnOnNetTest\" in app.conf, error: [%s]. We turn off the network performance test function.", err.Error()))
+	} else {
+		beego.Info("Network performance test function is off.")
 	}
-	beego.Info(fmt.Sprintf("The period of measuring network performance is %d seconds.", netTestPeriodSec))
-	go models.CronTaskTimer(models.MeasNetPerf, time.Duration(netTestPeriodSec)*time.Second)
 
 	beego.Run()
 }
