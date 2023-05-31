6ae4130704a7bc647601c18d18ca40cf2ddcdf61
Weifan Zhang <weifanzh@es.aau.dk>
2023-05-31 20:18:53 +0200

    Try to redirect the log into a log file and configure the auto log rotate in systemd, but as a result, I found that the default configuration is the best, so i commented the changes.

--- install_service.sh
+++ install_service.sh
@@ -1,16 +1,23 @@
 #!/bin/env bash
 
 CURRENT_DIR=$(cd $(dirname $0); pwd)
+LOG_DIR=$(cd ${CURRENT_DIR}/..; pwd)
+
+SVC_NAME="emcontroller"
 
 # give execution permission
-chmod +x ${CURRENT_DIR}/emcontroller
+chmod +x ${CURRENT_DIR}/${SVC_NAME}
 chmod +x ${CURRENT_DIR}/run.sh
 
 # render place holders. We should use | instead of / as the delimiter, because the the path contains /
-sed -i "s|\[\[PROJECTPATH\]\]|${CURRENT_DIR}|g" ${CURRENT_DIR}/emcontroller.service
+sed -i "s|\[\[PROJECTPATH\]\]|${CURRENT_DIR}|g" ${CURRENT_DIR}/${SVC_NAME}.service
+sed -i "s|\[\[LOGPATH\]\]|${LOG_DIR}|g" ${CURRENT_DIR}/${SVC_NAME}.service
 
 # install the service
-cp -f ${CURRENT_DIR}/emcontroller.service /usr/lib/systemd/system/emcontroller.service
+#cp -f ${CURRENT_DIR}/emcontroller.service /usr/lib/systemd/system/emcontroller.service
+cp -f ${CURRENT_DIR}/${SVC_NAME}.service /etc/systemd/system/${SVC_NAME}.service
+mkdir -p /etc/systemd/system/${SVC_NAME}.service.d
+cp -f ${CURRENT_DIR}/${SVC_NAME}-service.conf /etc/systemd/system/${SVC_NAME}.service.d/${SVC_NAME}-service.conf
 systemctl daemon-reload
-systemctl enable emcontroller
-systemctl start emcontroller
+systemctl enable ${SVC_NAME}
+systemctl start ${SVC_NAME}
--- uninstall_service.sh
+++ uninstall_service.sh
@@ -1,7 +1,11 @@
 #!/bin/env bash
 
-systemctl stop emcontroller
-systemctl disable emcontroller
-rm /usr/lib/systemd/system/emcontroller.service
+SVC_NAME="emcontroller"
+
+systemctl stop ${SVC_NAME}
+systemctl disable ${SVC_NAME}
+#rm /usr/lib/systemd/system/emcontroller.service
+rm /etc/systemd/system/${SVC_NAME}.service
+rm -rf /etc/systemd/system/${SVC_NAME}.service.d
 systemctl daemon-reload
 systemctl reset-failed
