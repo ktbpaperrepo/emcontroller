0ee2de7476933f805022e6ab5c61c8ec71ae1dfb
Weifan Zhang <weifanzh@es.aau.dk>
2023-09-22 15:06:13 +0200

    run response time experiments with 100 applications.

--- auto-schedule/experiments/response-time/executor-python/charts_drawer.py
+++ auto-schedule/experiments/response-time/executor-python/charts_drawer.py
@@ -8,7 +8,7 @@ import csv_operation
 
 REPEAT_COUNT = 1  # We repeat the experiments for REPEAT_COUNT times.
 DEVICE_COUNT = 1  # we use DEVICE_COUNT devices to send requests.
-APP_COUNT = 60  # In every repeat, we deploy APP_COUNT applications.
+APP_COUNT = 100  # In every repeat, we deploy APP_COUNT applications.
 REQ_COUNT_PER_APP = 5  # In every repeat, on every device, we access every application REQ_COUNT_PER_APP times.
 ALGO_NAMES = [
     "BERand", "Amaga", "Ampga", "Mcssga"
